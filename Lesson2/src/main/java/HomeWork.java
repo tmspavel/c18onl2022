e72dd8a4ed942022b036f319d9bceb01c75b10f7
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2022-10-25 00:42:41 +0300

    HomeWork 2

---
+++ Lesson2/src/main/java/HomeWork.java
@@ -0,0 +1,23 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        /*
+        0) В Идее ставим курсор на названии проекта С18onl2022 и создаем новый модуль(правой клавишей мыши new->module),
+        Название Lesson2, build system maven
+        1) В в папке Lesson2->java создать файл Test.java
+        2) Создать 2 дополнительные ветки features/TMS1, bugfix/TMS1
+        3) в ветке features/TMS1 изменить файл Test.java(добавляем вывод на консоль "Hello git")
+         и создать пулреквест ветки features/TMS1 к ветке develop(если не создали эту ветку то создайте и запуште)
+        Внимание!!! если вы создали ветку ее можно запушить сразу же без коммита.
+        4) Обновите ветку bugfix/TMS1 из develop(переключаемся на bugfix/TMS1,
+        кликаем на ветку develop и выбираем Merge into Current). Добавить 2 Test2.java и Test3.java в папку java,
+        закомитить и запушить их. Удалите Test2.java, сделать коммит и пуш и создать пулреквест
+        ветки bugfix/TMS1 к develop (при создании пул реквеста можно выбирать из какой ветки сливать изменения в какую ветку)
+        Итого: Должно быть 2 пул реквеста(features/TMS1 в develop и bugfix/TMS1 в develop)
+
+        5)*(Со звездой) Создать ветку features/TMS2 от main, далее перейти в ветку develop и добавить
+        файл с название Cherry.java закомитить и запушить.
+
+
+         */
+    }
+}
