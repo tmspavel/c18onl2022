2696ecb00dfedb77c08b081fe24bde48ddee52eb
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2022-11-09 23:35:40 +0300

    Lesson 5 практика

--- Lesson3/src/main/java/HomeWork.java
+++ Lesson3/src/main/java/HomeWork.java
@@ -9,6 +9,9 @@ public class HomeWork {
 
         System.out.println(max(56, 349));
         System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+        System.out.println(average(new int[]{-4, -2, -3, -1, -5}));
+        System.out.println(average(new int[]{}));
+        System.out.println(average(null));
         System.out.println(max(new int[]{1, 2, 3, 4, 5, 100, 99}));
         System.out.println(calculateHypotenuse(3, 4));
     }
@@ -18,11 +21,13 @@ public class HomeWork {
      * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
      **/
     public static int sum(int a, int b) {
+        if ((long) a + b > Integer.MAX_VALUE) {
+            return -1;
+        }
         return a + b;
     }
 
     /**
-     *
      * Метод должен вернуть максимальное значение из двух чисел
      *
      * <p>
@@ -38,7 +43,13 @@ public class HomeWork {
      * Метод должен вернуть 10
      */
     public static int max(int a, int b) {
-        return 0;
+//        return (a >= b) ? a : b;
+        if (a >= b) {
+            return a;
+        } else {
+            return b;
+        }
+//        return Math.max(a, b);
     }
 
     /**
@@ -50,19 +61,42 @@ public class HomeWork {
      * Метод должен return 3.0
      */
     public static double average(int[] array) {
-        return 0;
+        double result = 0;
+        if (array != null) {
+            for (int value : array) {
+                result += value;
+            }
+            return result / array.length;
+        }
+        return result;
     }
 
     /**
      * Метод должен вернуть максимальый элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
      **/
     public static int max(int[] array) {
+        if (array != null && array.length > 0) {
+//            int result = Integer.MIN_VALUE;
+            int result = array[0];
+            for (int i = 1; i < array.length; i++) {
+                int value = array[i];
+                if (value > result) {
+                    result = value;
+                }
+            }
+//            for (int value : array) {
+//                if (value > result) {
+//                    result = value;
+//                }
+//            }
+            return result;
+        }
         return 0;
     }
 
     /**
      * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
-     *
+     * <p>
      * Example1:
      * 3
      * 4
@@ -74,6 +108,6 @@ public class HomeWork {
      * return 20
      */
     public static double calculateHypotenuse(int a, int b) {
-        return 0;
+        return Math.hypot(a, b);
     }
 }
