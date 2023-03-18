5a1bd56bce75a9644e316b95e99d7dbc201c396c
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2023-03-19 02:53:15 +0300

    Lesson28 fix factory

--- Lesson28hw/src/main/java/by/teachmeskills/eshop/utils/CommandFactory.java
+++ Lesson28hw/src/main/java/by/teachmeskills/eshop/utils/CommandFactory.java
@@ -5,28 +5,36 @@ import by.teachmeskills.eshop.controller.HomeController;
 import by.teachmeskills.eshop.controller.LogoutController;
 import by.teachmeskills.eshop.model.Command;
 import by.teachmeskills.eshop.model.RequestParam;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 import javax.servlet.http.HttpServletRequest;
 
 public class CommandFactory {
 
-    private static final Map<String, BaseController> COMMANDS = new HashMap<>();
+    private static final Map<String, BaseController> COMMANDS = new ConcurrentHashMap<>();
 
     public static BaseController defineCommand(HttpServletRequest request) {
         String commandKey = request.getParameter(RequestParam.COMMAND.getValue());
         if (commandKey == null || commandKey.isEmpty()) {
             commandKey = Command.SIGN_IN_COMMAND.getCommand();
         }
-        return COMMANDS.putIfAbsent(commandKey, createController(Command.fromString(commandKey)));
+        return putIfAbsent(commandKey, createController(Command.fromString(commandKey)));
     }
 
-    private static BaseController createController(Command command) {
+    private static Supplier<BaseController> createController(Command command) {
         return switch (command) {
-            case SIGN_IN_COMMAND -> new HomeController();
-            case LOGOUT_COMMAND -> new LogoutController();
+            case SIGN_IN_COMMAND -> HomeController::new;
+            case LOGOUT_COMMAND -> LogoutController::new;
         };
     }
 
+    private static BaseController putIfAbsent(String key, Supplier<BaseController> supplier) {
+        BaseController value = COMMANDS.get(key);
+        if (value == null) {
+            COMMANDS.put(key, value = supplier.get());
+        }
+        return value;
+    }
 }
 
